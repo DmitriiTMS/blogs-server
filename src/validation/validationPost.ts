b2a822eaab799c32838211d6157e198b4d1abb9e
DmitriiTMS <dmitrii12_88@mail.ru>
2025-03-06 21:04:44 +0300

    fix validation

--- src/validation/validationPost.ts
+++ src/validation/validationPost.ts
@@ -3,11 +3,11 @@ import { body } from "express-validator";
 const titleValidation = body("title")
     .exists()
     .withMessage("Title is required")
+    .trim()
     .notEmpty()
     .withMessage("Title cannot be empty")
     .isString()
     .withMessage("The Title field must be a string")
-    .trim()
     .isLength({ max: 30 })
     .withMessage("The Title field must be a maximum of 30 characters");
 
@@ -15,22 +15,22 @@ const titleValidation = body("title")
 const shortDescriptionValidation = body("shortDescription")
     .exists()
     .withMessage("ShortDescription is required")
+    .trim()
     .notEmpty()
     .withMessage("ShortDescription cannot be empty")
     .isString()
     .withMessage("The ShortDescription field must be a string")
-    .trim()
     .isLength({ max: 100 })
     .withMessage("The ShortDescription field must be a maximum of 100 characters");
 
 const contentValidation = body("content")
     .exists()
     .withMessage("Content is required")
+    .trim()
     .notEmpty()
     .withMessage("Content cannot be empty")
     .isString()
     .withMessage("The Content field must be a string")
-    .trim()
     .isLength({ max: 1000 })
     .withMessage("The Content field must be a maximum of 1000 characters");
 
@@ -38,11 +38,11 @@ const contentValidation = body("content")
 const blogIdValidation = body("blogId")
     .exists()
     .withMessage("BlogId is required")
+    .trim()
     .notEmpty()
     .withMessage("BlogId cannot be empty")
     .isString()
     .withMessage("The BlogId field must be a string")
-    .trim()
 
 
 export const fieldValidationPost = [
