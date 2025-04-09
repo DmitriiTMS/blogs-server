384f10d2eeb12e572bb918da285d641715dd5e08
DmitriiTMS <dmitrii12_88@mail.ru>
2025-04-09 19:16:31 +0300

    add tests support

--- __tests__/auth.e2e.test.ts
+++ __tests__/auth.e2e.test.ts
@@ -6,7 +6,6 @@ import { closeDB, runDB, usersCollection } from "../src/db/mongoDB";
 import { usersRepository } from "../src/repository/users/usersRepository";
 
 describe("/auth", () => {
-    
   beforeAll(async () => {
     const mongoServer = await MongoMemoryServer.create();
     const uri = mongoServer.getUri();
@@ -14,12 +13,12 @@ describe("/auth", () => {
     await request(app).delete("/testing/all-data").expect(204);
 
     const user = {
-        login: 'user1',
-        email: 'user1@mail.ru',
-        password: '123456'
-    }
+      login: "user1",
+      email: "user1@mail.ru",
+      password: "123456",
+    };
 
-    await usersRepository.createUser(user)
+    await usersRepository.createUser(user);
   });
 
   afterAll(async () => {
@@ -28,25 +27,76 @@ describe("/auth", () => {
 
   it("login: if user not found --- LOGIN", async () => {
     const user = {
-        login: 'user2',
-        email: 'user1@mail.ru',
-        password: '123456'
-    }
+      login: "user2",
+      email: "user1@mail.ru",
+      password: "123456",
+    };
     await request(app)
       .post(`${SETTINGS.PATH.AUTH}/login`)
-      .send({ loginOrEmail: user.login,  password: user.password })
+      .send({ loginOrEmail: user.login, password: user.password })
       .expect(401);
   });
 
   it("email: if user not found --- EMAIL", async () => {
     const user = {
-        login: 'user1',
-        email: 'user2@mail.ru',
-        password: '123456'
-    }
+      login: "user1",
+      email: "user2@mail.ru",
+      password: "123456",
+    };
     await request(app)
       .post(`${SETTINGS.PATH.AUTH}/login`)
-      .send({ loginOrEmail: user.email,  password: user.password })
+      .send({ loginOrEmail: user.email, password: user.password })
       .expect(401);
+  });
+
+  it("get me not token", async () => {
+    await request(app)
+      .get(`${SETTINGS.PATH.AUTH}/me`)
+      .auth("", { type: "bearer" })
+      .expect(401);
+
+    await request(app)
+      .get(`${SETTINGS.PATH.AUTH}/me`)
+      .auth("wefwafw13134134", { type: "bearer" })
+      .expect(401);
+  });
+  it("user refresh page", async () => {
+    const user = {
+      login: "user23231",
+      email: "user2424@mail.ru",
+      password: "123456",
+    };
+
+    await request(app)
+      .post(`${SETTINGS.PATH.USERS}`)
+      .auth("admin", "qwerty")
+      .send({
+        login: user.login,
+        email: user.email,
+        password: user.password,
+      })
+      .expect(201);
+
+    const resLoginUser = await request(app)
+      .post(`${SETTINGS.PATH.AUTH}/login`)
+      .send({
+        loginOrEmail: user.login,
+        password: user.password,
+      })
+      .expect(200);
+
+    const accessToken = resLoginUser.body.accessToken;
+    // const refreshToken = resLoginUser.headers["set-cookie"];
+
+    const userInfo = await request(app)
+      .get(`${SETTINGS.PATH.AUTH}/me`)
+      .auth(accessToken, { type: "bearer" })
+      .expect(200);
+
+      expect(userInfo.body).toEqual({
+        userId: expect.any(String),
+        email: user.email,
+        login: user.login
+      })
   });
 });
